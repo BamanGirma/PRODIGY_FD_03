b4392dff70373c5ece7494dc3c4cba36fb4afcea
BamanGirma <amanuelgirma360@gmail.com>
2024-09-18 14:57:05 +0300

    header and carousel only

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Header/Header'
+import Carousel from './Components/Carousel/CarouselEffect'
+import CarouselEffect from './Components/Carousel/CarouselEffect'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <CarouselEffect/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Carousel/CarouselEffect.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Carousel} from 'react-responsive-carousel' 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {img} from './images/data'
+import classes from './Carousel.module.css'
+function CarouselEffect() {
+  return (
+    <div>
+        <Carousel
+        autoplay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+        >
+
+            {
+                img.map((imageItemLink) =>{
+                    return <img src={imageItemLink}/>
+                } )
+            }
+        </Carousel>
+        <div
+        className={classes.hero__img}>
+
+        </div>
+    </div>
+  )
+}
+
+export default CarouselEffect
